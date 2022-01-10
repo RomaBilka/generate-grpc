22b14ef729670b2636a56609baf72da9776329c6
Roman Biliak <roman_biliak@epam.com>
2022-01-10 16:05:08 +0200

    add database

---
+++ database/query/items.sql
@@ -0,0 +1,31 @@
+-- name: CreateItem :one
+INSERT INTO items (name, value)
+VALUES (@name, @value)
+RETURNING id;
+
+-- name: UpdateItem :exec
+UPDATE items SET name = @name, value = @value
+WHERE id = @id;
+
+-- name: GetItemById :one
+SELECT *
+FROM items
+WHERE id = @id;
+
+-- name: GetItemByName :one
+SELECT *
+FROM items
+WHERE name = @name;
+
+-- name: CheckItemExists :one
+SELECT id
+FROM items
+WHERE name = @name;
+
+-- name: DeleteItem :exec
+DELETE FROM items
+WHERE id = @id;
+
+-- name: GetItems :many
+SELECT *
+FROM items;
