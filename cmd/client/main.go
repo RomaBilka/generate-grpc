3820ae331df43bb9812148c9ae7dbb9fb4d21470
Roman Biliak <roman_biliak@epam.com>
2022-01-11 11:45:46 +0200

    client

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -18,11 +18,27 @@ func main(){
 
 	client := grpc_item.NewCRUDClient(conn)
 
-	response, err := client.CreateItem(context.Background(), &grpc_item.Item{Name: "test", Value: "test_value"})
-	fmt.Println(response)
+	itemId, err := client.CreateItem(context.Background(), &grpc_item.Item{Name: "test", Value: "test_value"})
+	fmt.Println(itemId)
 	fmt.Println(err)
 
-	response, err = client.GetItem(context.Background(), &grpc_item.ItemId{Id:1})
-	fmt.Println(response)
+	item, err := client.GetItem(context.Background(), &grpc_item.ItemId{Id:1}, )
+	fmt.Println(item)
+	fmt.Println(err)
+
+	items, err := client.GetItems(context.Background(),  &grpc_item.Void{})
+	fmt.Println(items.GetAliases())
+	fmt.Println(err)
+
+	itemId, err = client.DeleteItem(context.Background(), &grpc_item.ItemId{Id:1}, )
+	fmt.Println(itemId)
+	fmt.Println(err)
+
+	itemId, err = client.UpdateItem(context.Background(), &grpc_item.Item{Id: 2, Name: "new_test", Value: "test_value"})
+	fmt.Println(itemId)
+	fmt.Println(err)
+
+	items, err = client.GetItems(context.Background(),  &grpc_item.Void{})
+	fmt.Println(items.GetAliases())
 	fmt.Println(err)
 }
